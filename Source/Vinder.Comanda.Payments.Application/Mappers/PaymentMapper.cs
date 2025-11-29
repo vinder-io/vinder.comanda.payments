fba09b2e26fa75cd3021b1c4b278249b0fcd23ed
Richard Garcia <code.richardy@gmail.com>
2025-11-29 02:00:05 -0300

    this commit introduces a mapping extension to convert offline payment charge data into a payment entity

--- Source/Vinder.Comanda.Payments.Application/Mappers/PaymentMapper.cs
+++ Source/Vinder.Comanda.Payments.Application/Mappers/PaymentMapper.cs
@@ -6,8 +6,26 @@ public static class PaymentMapper
     {
         Amount = parameters.Amount,
         Status = Status.Pending,
+        Method = Method.Pix,
         Metadata = new PaymentMetadata(Identifier: string.Empty, Reference: parameters.Reference),
         Payer = new User(parameters.Payer.Identifier, parameters.Payer.Username),
+    };
+
+    public static Payment AsPayment(this OfflinePaymentChargeScheme parameters) => new()
+    {
+        Amount = parameters.Amount,
+        Method = parameters.Method,
+        Status = Status.Paid,
+
+        Payer = new User(
+            Identifier: string.Empty,
+            Username: string.Empty
+        ),
+
+        Metadata = new PaymentMetadata(
+            Identifier: string.Empty,
+            Reference: parameters.Reference
+        )
     };
 
     public static PaymentScheme AsResponse(this Payment payment) => new()
@@ -16,6 +34,7 @@ public static class PaymentMapper
         Reference = payment.Metadata.Reference,
         Amount = payment.Amount,
         Payer = payment.Payer,
-        Status = payment.Status
+        Status = payment.Status,
+        Method = payment.Method
     };
 }
