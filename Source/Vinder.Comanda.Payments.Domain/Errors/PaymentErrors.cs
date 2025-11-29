74051fd2d09b87655381e724114a03b1df82baa2
Richard Garcia <code.richardy@gmail.com>
2025-11-29 02:22:05 -0300

    this commit introduces a generic error for disallowed payment methods to enforce business rules

---
+++ Source/Vinder.Comanda.Payments.Domain/Errors/PaymentErrors.cs
@@ -0,0 +1,9 @@
+﻿namespace Vinder.Comanda.Payments.Domain.Errors;
+
+public static class PaymentErrors
+{
+    public static readonly Error MethodNotAllowed = new(
+        Code: "#COMANDA-ERROR-947B5",
+        Description: "the specified payment method is not allowed for this type of transaction"
+    );
+}
