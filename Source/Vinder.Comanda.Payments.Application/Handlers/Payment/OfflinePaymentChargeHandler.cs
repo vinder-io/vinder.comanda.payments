6f1b17f2e5e4fdb9e4c49dac5212360a73704473
Richard Garcia <code.richardy@gmail.com>
2025-11-29 01:59:35 -0300

    this commit introduces a handler to process offline payments and return the corresponding result

---
+++ Source/Vinder.Comanda.Payments.Application/Handlers/Payment/OfflinePaymentChargeHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Vinder.Comanda.Payments.Application.Handlers.Payment;
+
+public sealed class OfflinePaymentChargeHandler(IPaymentRepository repository) :
+    IMessageHandler<OfflinePaymentChargeScheme, Result<PaymentScheme>>
+{
+    public async Task<Result<PaymentScheme>> HandleAsync(
+        OfflinePaymentChargeScheme parameters, CancellationToken cancellation = default)
+    {
+        var payment = await repository.InsertAsync(parameters.AsPayment(), cancellation);
+
+        return Result<PaymentScheme>.Success(payment.AsResponse());
+    }
+}
