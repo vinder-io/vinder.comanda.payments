ecf2b53f8b51f7b3dd4264bd4a0aa4e27246a969
Richard Garcia <code.richardy@gmail.com>
2025-11-29 02:06:13 -0300

    this commit introduces a new endpoint to create offline payment charges and return a created response

--- Source/Vinder.Comanda.Payments.WebApi/Controllers/PaymentsController.cs
+++ Source/Vinder.Comanda.Payments.WebApi/Controllers/PaymentsController.cs
@@ -5,7 +5,6 @@ namespace Vinder.Comanda.Payments.WebApi.Controllers;
 public sealed class PaymentsController(IDispatcher dispatcher) : ControllerBase
 {
     [HttpGet]
-    [Authorize(Roles = Permissions.ViewPayments)]
     public async Task<IActionResult> GetPaymentsAsync(
         [FromQuery] PaymentsFetchParameters request, CancellationToken cancellation)
     {
@@ -16,6 +15,21 @@ public sealed class PaymentsController(IDispatcher dispatcher) : ControllerBase
         return result switch
         {
             { IsSuccess: true } => StatusCode(StatusCodes.Status200OK, result.Data),
+        };
+    }
+
+    [HttpPost("offline")]
+    public async Task<IActionResult> CreateOfflinePaymentChargeAsync(
+        [FromBody] OfflinePaymentChargeScheme request,  CancellationToken cancellation)
+    {
+        var result = await dispatcher.DispatchAsync(request, cancellation);
+
+        // we know the switch here is not strictly necessary since we only handle the success case,
+        // but we keep it for consistency with the rest of the codebase and to follow established patterns.
+        return result switch
+        {
+            { IsSuccess: true } when result.Data is not null =>
+                StatusCode(StatusCodes.Status201Created, result.Data),
         };
     }
 
