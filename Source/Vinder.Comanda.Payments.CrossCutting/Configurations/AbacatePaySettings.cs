c8d13196f0eb8869b1c2ae0d0f2ee8a997d11c10
Richard Garcia <code.richardy@gmail.com>
2026-01-02 16:12:30 -0300

    this commit removes unused credential property from abacate pay settings record

--- Source/Vinder.Comanda.Payments.CrossCutting/Configurations/AbacatePaySettings.cs
+++ Source/Vinder.Comanda.Payments.CrossCutting/Configurations/AbacatePaySettings.cs
@@ -3,5 +3,4 @@ namespace Vinder.Comanda.Payments.CrossCutting.Configurations;
 public sealed record AbacatePaySettings
 {
     public string Url { get; init; } = default!;
-    public string Credential { get; init; } = default!;
 }
