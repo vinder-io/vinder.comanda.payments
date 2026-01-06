9544b22158063f41edcccada436f6e52d20f99f3
Richard Garcia <code.richardy@gmail.com>
2026-01-06 20:26:23 -0300

    refactor: remove unused Permissions class from cross-cutting constants

--- Source/Vinder.Comanda.Payments.CrossCutting/Constants/Permissions.cs
+++
@@ -1,9 +0,0 @@
-namespace Vinder.Comanda.Payments.CrossCutting.Constants;
-
-public static class Permissions
-{
-    public const string ViewActivities = "permissions.activities.view";
-    public const string ViewPayments = "permissions.payments.view";
-    public const string MakePayment = "permissions.payments.make";
-    public const string PaymentUpdate = "permissions.payments.update";
-}
